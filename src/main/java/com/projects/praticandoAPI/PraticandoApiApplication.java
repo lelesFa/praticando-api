a6c9d782019b90de38eaa6cc170c120f09e5a5e4
Andreia Leles <leles.andreia@gmail.com>
2021-05-18 17:58:15 -0300

    como jar

--- src/main/java/com/projects/praticandoAPI/PraticandoApiApplication.java
+++ src/main/java/com/projects/praticandoAPI/PraticandoApiApplication.java
@@ -2,8 +2,7 @@ package com.projects.praticandoAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Controller
 @SpringBootApplication
 @EnableSwagger2
-public class PraticandoApiApplication extends SpringBootServletInitializer {
+public class PraticandoApiApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PraticandoApiApplication.class, args);
@@ -27,9 +26,5 @@ public class PraticandoApiApplication extends SpringBootServletInitializer {
 		return "Rodando a aplicacao";
 		}
 	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(PraticandoApiApplication.class);
-	}
-
+	
 }
